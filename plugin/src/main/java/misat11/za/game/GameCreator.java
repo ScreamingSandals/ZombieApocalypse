8affd911fd4fcb036c781c316dba0ef5e567f4a9
Misat11 <misa.turek1@gmail.com>
2019-07-18 18:05:03 +0200

    changed from enum to strings

--- plugin/src/main/java/misat11/za/game/GameCreator.java
+++ plugin/src/main/java/misat11/za/game/GameCreator.java
@@ -59,7 +59,7 @@ public class GameCreator {
 	}
 
 	public void cmd(Player player, String action, String[] args) {
-		CommandResponse response = null;
+		String response = null;
 		if (action.equalsIgnoreCase("spawn")) {
 			response = setSpawn(player.getLocation());
 		} else if (action.equalsIgnoreCase("pos1")) {
@@ -151,195 +151,195 @@ public class GameCreator {
 			game.saveToConfig();
 			game.start();
 			Main.addGame(game);
-			response = CommandResponse.SAVED_AND_STARTED;
+			response = i18n("admin_command_game_saved_and_started");
 		}
 
 		if (response == null) {
-			response = CommandResponse.UNKNOWN_COMMAND;
+			response = i18n("unknown_command");
 		}
-		player.sendMessage(response.i18n());
+		player.sendMessage(response);
 	}
 
-	public CommandResponse setBossGame(Location loc) {
+	public String setBossGame(Location loc) {
 		if (game.getWorld() != loc.getWorld()) {
-			return CommandResponse.MUST_BE_IN_SAME_WORLD;
+			return i18n("admin_command_must_be_in_same_world");
 		}
 		game.setBoss(loc);
-		return CommandResponse.SUCCESS;
+		return i18n("admin_command_success");
 	}
 
-	public CommandResponse resetBossGame() {
+	public String resetBossGame() {
 		game.setBoss(null);
-		return CommandResponse.SUCCESS;
+		return i18n("admin_command_success");
 	}
 
-	public CommandResponse addStore(Location loc) {
+	public String addStore(Location loc) {
 		if (game.getWorld() != loc.getWorld()) {
-			return CommandResponse.MUST_BE_IN_SAME_WORLD;
+			return i18n("admin_command_must_be_in_same_world");
 		}
 		String location = loc.getBlockX() + ";" + loc.getBlockY() + ";" + loc.getBlockZ();
 		if (villagerstores.contains(location)) {
-			return CommandResponse.STORE_EXISTS;
+			return i18n("admin_command_store_already_exists");
 		}
 		villagerstores.add(location);
-		return CommandResponse.SUCCESS;
+		return i18n("admin_command_success");
 	}
 
-	public CommandResponse removeStore(Location loc) {
+	public String removeStore(Location loc) {
 		String location = loc.getBlockX() + ";" + loc.getBlockY() + ";" + loc.getBlockZ();
 		if (!villagerstores.contains(location)) {
-			return CommandResponse.STORE_NOT_EXISTS;
+			return i18n("admin_command_store_not_exists");
 		}
 		villagerstores.remove(location);
-		return CommandResponse.SUCCESS;
+		return i18n("admin_command_success");
 	}
 
-	public CommandResponse addSmallArena(String name) {
+	public String addSmallArena(String name) {
 		if (smallarenas.containsKey(name)) {
-			return CommandResponse.SMALL_ALREADY_EXISTS;
+			return i18n("admin_command_small_already_exists");
 		}
 		smallarenas.put(name, new SmallArena(name, null, null));
-		return CommandResponse.SUCCESS;
+		return i18n("admin_command_success");
 	}
 
-	public CommandResponse removeSmallArena(String name) {
+	public String removeSmallArena(String name) {
 		if (!smallarenas.containsKey(name)) {
-			return CommandResponse.SMALL_NOT_EXISTS;
+			return i18n("admin_command_small_not_exists");
 		}
 		smallarenas.remove(name);
-		return CommandResponse.SUCCESS;
+		return i18n("admin_command_success");
 	}
 
-	public CommandResponse pos1SmallArena(String name, Location loc) {
+	public String pos1SmallArena(String name, Location loc) {
 		if (!smallarenas.containsKey(name)) {
-			return CommandResponse.SMALL_NOT_EXISTS;
+			return i18n("admin_command_small_not_exists");
 		}
 		if (game.getWorld() != loc.getWorld()) {
-			return CommandResponse.MUST_BE_IN_SAME_WORLD;
+			return i18n("admin_command_must_be_in_same_world");
 		}
 		smallarenas.get(name).pos1 = loc;
-		return CommandResponse.SUCCESS;
+		return i18n("admin_command_success");
 	}
 
-	public CommandResponse pos2SmallArena(String name, Location loc) {
+	public String pos2SmallArena(String name, Location loc) {
 		if (!smallarenas.containsKey(name)) {
-			return CommandResponse.SMALL_NOT_EXISTS;
+			return i18n("admin_command_small_not_exists");
 		}
 		if (game.getWorld() != loc.getWorld()) {
-			return CommandResponse.MUST_BE_IN_SAME_WORLD;
+			return i18n("admin_command_must_be_in_same_world");
 		}
 		smallarenas.get(name).pos2 = loc;
-		return CommandResponse.SUCCESS;
+		return i18n("admin_command_success");
 	}
 
-	public CommandResponse setSpawn(Location loc) {
+	public String setSpawn(Location loc) {
 		if (game.getPos1() == null || game.getPos2() == null) {
-			return CommandResponse.SET_POS1_POS2_FIRST;
+			return i18n("admin_command_set_pos1_pos2_first");
 		}
 		if (game.getWorld() != loc.getWorld()) {
-			return CommandResponse.MUST_BE_IN_SAME_WORLD;
+			return i18n("admin_command_must_be_in_same_world");
 		}
 		if (!isInArea(loc, game.getPos1(), game.getPos2())) {
-			return CommandResponse.SPAWN_MUST_BE_IN_MAIN_AREA;
+			return i18n("admin_command_spawn_must_be_in_area");
 		}
 		game.setSpawn(loc);
-		return CommandResponse.SUCCESS;
+		return i18n("admin_command_success");
 	}
 
-	public CommandResponse setPos1(Location loc) {
+	public String setPos1(Location loc) {
 		if (game.getWorld() == null) {
 			game.setWorld(loc.getWorld());
 		}
 		if (game.getWorld() != loc.getWorld()) {
-			return CommandResponse.MUST_BE_IN_SAME_WORLD;
+			return i18n("admin_command_must_be_in_same_world");
 		}
 		if (game.getPos2() != null) {
 			if (Math.abs(game.getPos2().getBlockY() - loc.getBlockY()) <= 5) {
-				return CommandResponse.POS1_POS2_DIFFERENCE_MUST_BE_HIGHER;
+				return i18n("admin_command_pos1_pos2_difference_must_be_higher");
 			}
 		}
 		game.setPos1(loc);
-		return CommandResponse.SUCCESS;
+		return i18n("admin_command_success");
 	}
 
-	public CommandResponse setPos2(Location loc) {
+	public String setPos2(Location loc) {
 		if (game.getWorld() == null) {
 			game.setWorld(loc.getWorld());
 		}
 		if (game.getWorld() != loc.getWorld()) {
-			return CommandResponse.MUST_BE_IN_SAME_WORLD;
+			return i18n("admin_command_must_be_in_same_world");
 		}
 		if (game.getPos1() != null) {
 			if (Math.abs(game.getPos1().getBlockY() - loc.getBlockY()) <= 5) {
-				return CommandResponse.POS1_POS2_DIFFERENCE_MUST_BE_HIGHER;
+				return i18n("admin_command_pos1_pos2_difference_must_be_higher");
 			}
 		}
 		game.setPos2(loc);
-		return CommandResponse.SUCCESS;
+		return i18n("admin_command_success");
 	}
 
-	public CommandResponse setPauseCountdown(int countdown) {
+	public String setPauseCountdown(int countdown) {
 		if (countdown >= 10 && countdown <= 600) {
 			game.setPauseCountdown(countdown);
-			return CommandResponse.SUCCESS;
+			return i18n("admin_command_success");
 		}
-		return CommandResponse.INVALID_COUNTDOWN;
+		return i18n("admin_command_invalid_countdown");
 	}
 
-	public CommandResponse addPhase(Player player, int index, int countdown) {
+	public String addPhase(Player player, int index, int countdown) {
 		PhaseInfo info = new PhaseInfo(countdown);
 		phases.add(index, info);
 		player.sendMessage(i18n("admin_phase_added_number", false).replace("%number%", Integer.toString(index)));
-		return CommandResponse.SUCCESS;
+		return i18n("admin_command_success");
 	}
 
-	public CommandResponse addPhase(Player player, int countdown) {
+	public String addPhase(Player player, int countdown) {
 		PhaseInfo info = new PhaseInfo(countdown);
 		phases.add(info);
 		player.sendMessage(
 				i18n("admin_phase_added_number", false).replace("%number%", Integer.toString(phases.size() - 1)));
-		return CommandResponse.SUCCESS;
+		return i18n("admin_command_success");
 	}
 
-	public CommandResponse removePhase(int index) {
+	public String removePhase(int index) {
 		if (index < 0 || index >= phases.size()) {
-			return CommandResponse.PHASE_NOT_EXISTS;
+			return i18n("admin_command_phase_not_exists");
 		}
 		if (phases.get(index) == null) {
-			return CommandResponse.PHASE_NOT_EXISTS;
+			return i18n("admin_command_phase_not_exists");
 		}
 		phases.remove(index);
-		return CommandResponse.SUCCESS;
+		return i18n("admin_command_success");
 	}
 
-	public CommandResponse editPhaseCountdown(int index, int countdown) {
+	public String editPhaseCountdown(int index, int countdown) {
 		if (index < 0 || index >= phases.size()) {
-			return CommandResponse.PHASE_NOT_EXISTS;
+			return i18n("admin_command_phase_not_exists");
 		}
 		if (phases.get(index) == null) {
-			return CommandResponse.PHASE_NOT_EXISTS;
+			return i18n("admin_command_phase_not_exists");
 		}
 		phases.get(index).setCountdown(countdown);
-		return CommandResponse.SUCCESS;
+		return i18n("admin_command_success");
 	}
 
-	public CommandResponse addMonster(int index, EntityType monster, int spawnCountdown) {
+	public String addMonster(int index, EntityType monster, int spawnCountdown) {
 		if (index < 0 || index >= phases.size()) {
-			return CommandResponse.PHASE_NOT_EXISTS;
+			return i18n("admin_command_phase_not_exists");
 		}
 		if (phases.get(index) == null) {
-			return CommandResponse.PHASE_NOT_EXISTS;
+			return i18n("admin_command_phase_not_exists");
 		}
 		phases.get(index).addMonster(new MonsterInfo(spawnCountdown, monster));
-		return CommandResponse.SUCCESS;
+		return i18n("admin_command_success");
 	}
 
-	public CommandResponse removeMonster(int index, EntityType monster) {
+	public String removeMonster(int index, EntityType monster) {
 		if (index < 0 || index >= phases.size()) {
-			return CommandResponse.PHASE_NOT_EXISTS;
+			return i18n("admin_command_phase_not_exists");
 		}
 		if (phases.get(index) == null) {
-			return CommandResponse.PHASE_NOT_EXISTS;
+			return i18n("admin_command_phase_not_exists");
 		}
 		List<MonsterInfo> mi = new ArrayList<MonsterInfo>((phases.get(index).getMonsters()));
 		for (MonsterInfo info : mi) {
@@ -348,19 +348,19 @@ public class GameCreator {
 				break;
 			}
 		}
-		return CommandResponse.SUCCESS;
+		return i18n("admin_command_success");
 
 	}
 
-	public CommandResponse addSmallMonster(String small, int index, EntityType monster, int spawnCountdown) {
+	public String addSmallMonster(String small, int index, EntityType monster, int spawnCountdown) {
 		if (!smallarenas.containsKey(small)) {
-			return CommandResponse.SMALL_NOT_EXISTS;
+			return i18n("admin_command_small_not_exists");
 		}
 		if (index < 0 || index >= phases.size()) {
-			return CommandResponse.PHASE_NOT_EXISTS;
+			return i18n("admin_command_phase_not_exists");
 		}
 		if (phases.get(index) == null) {
-			return CommandResponse.PHASE_NOT_EXISTS;
+			return i18n("admin_command_phase_not_exists");
 		}
 		PhaseInfo phase = phases.get(index);
 		SmallArena smallarena = smallarenas.get(small);
@@ -369,18 +369,18 @@ public class GameCreator {
 		}
 		List<MonsterInfo> list = smallarena.monsters.get(phase);
 		list.add(new MonsterInfo(spawnCountdown, monster));
-		return CommandResponse.SUCCESS;
+		return i18n("admin_command_success");
 	}
 
-	public CommandResponse removeSmallMonster(String small, int index, EntityType monster) {
+	public String removeSmallMonster(String small, int index, EntityType monster) {
 		if (!smallarenas.containsKey(small)) {
-			return CommandResponse.SMALL_NOT_EXISTS;
+			return i18n("admin_command_small_not_exists");
 		}
 		if (index < 0 || index >= phases.size()) {
-			return CommandResponse.PHASE_NOT_EXISTS;
+			return i18n("admin_command_phase_not_exists");
 		}
 		if (phases.get(index) == null) {
-			return CommandResponse.PHASE_NOT_EXISTS;
+			return i18n("admin_command_phase_not_exists");
 		}
 		PhaseInfo phase = phases.get(index);
 		SmallArena smallarena = smallarenas.get(small);
@@ -394,7 +394,7 @@ public class GameCreator {
 				break;
 			}
 		}
-		return CommandResponse.SUCCESS;
+		return i18n("admin_command_success");
 
 	}
 
@@ -405,31 +405,5 @@ public class GameCreator {
 				Math.max(p1.getZ(), p2.getZ()));
 		return (min.getX() <= l.getX() && min.getY() <= l.getY() && min.getZ() <= l.getZ() && max.getX() >= l.getX()
 				&& max.getY() >= l.getY() && max.getZ() >= l.getZ());
-	}
-
-	enum CommandResponse {
-		SUCCESS("admin_command_success"), MUST_BE_IN_SAME_WORLD(
-				"admin_command_must_be_in_same_world"), INVALID_COUNTDOWN(
-						"admin_command_invalid_countdown"), SPAWN_MUST_BE_IN_MAIN_AREA(
-								"admin_command_spawn_must_be_in_area"), SET_POS1_POS2_FIRST(
-										"admin_command_set_pos1_pos2_first"), POS1_POS2_DIFFERENCE_MUST_BE_HIGHER(
-												"admin_command_pos1_pos2_difference_must_be_higher"), PHASE_NOT_EXISTS(
-														"admin_command_phase_not_exists"), UNKNOWN_COMMAND(
-																"unknown_command"), SAVED_AND_STARTED(
-																		"admin_command_game_saved_and_started"), SMALL_ALREADY_EXISTS(
-																				"admin_command_small_already_exists"), SMALL_NOT_EXISTS(
-																						"admin_command_small_not_exists"), STORE_EXISTS(
-																								"admin_command_store_already_exists"), STORE_NOT_EXISTS(
-																										"admin_command_store_not_exists");
-
-		private final String msg;
-
-		private CommandResponse(String msg) {
-			this.msg = msg;
-		}
-
-		public String i18n() {
-			return i18nonly(msg);
-		}
 	}
 }
